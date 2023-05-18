db10193d17de9ba8df550e61e88d8c1798a0c376
diegosantarelli <s1099386@studenti.univpm.it>
2023-05-18 11:35:52 +0200

    modifiche catalogo.css

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -3,14 +3,14 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/Catalogo.css')}}">
     <div id="container">
 
-        <div class="search_container">
-        <form action="{{ route('search')}}" method="GET">
-                <label for="oggetto">Oggetto:</label><input id="obj" type="text" name="oggetto" placeholder="Inserisci l'oggetto dell'offerta">
-                <label for="azienda">Azienda:</label><input id="azienda" type="text" name="azienda" placeholder="Inserisci l'azienda dell'offerta">
-                <button id="button" type="submit">Cerca</button>
-        </form> 
-        </div>
+    <div class="search_container">
+    <form action="{{ route('search')}}" method="GET">
+            <label for="oggetto">Oggetto:</label><input id="obj" type="text" name="oggetto" placeholder="Inserisci l'oggetto dell'offerta">
+            <label for="azienda">Azienda:</label><input id="azienda" type="text" name="azienda" placeholder="Inserisci l'azienda dell'offerta">
+            <button id="button" type="submit">Cerca</button>
+    </form> 
     </div>
+    
 
 
     <div id="categorie">
