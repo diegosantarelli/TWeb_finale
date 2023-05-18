73e0cd61684d7431d75c51b319f5e65f77661778
diegosantarelli <s1099386@studenti.univpm.it>
2023-05-18 11:22:17 +0200

    catalogo.blade.php e catalogo.css

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -2,14 +2,15 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="{{asset('css/Catalogo.css')}}">
     <div id="container">
-     
+    
+    <h2 class="rule">puo ricercare un'offerta per oggetto <b>E/O</b> per azienda</h2>
+
     <div class="search_container">
-        <h2>puo ricercare un'offerta per oggetto <b>E/O</b> per azienda</h2>
     <form action="{{ route('search')}}" method="GET">
-            <label for="oggetto">Oggetto:</label><input type="text" name="oggetto" placeholder="Inserisci l'oggetto dell'offerta">
-            <label for="azienda">Azienda:</label><input type="text" name="azienda" placeholder="Inserisci l'azienda dell'offerta">
-            <button type="submit">Cerca</button>
-        </form> 
+            <label for="oggetto">Oggetto:</label><input id="obj" type="text" name="oggetto" placeholder="Inserisci l'oggetto dell'offerta">
+            <label for="azienda">Azienda:</label><input id="azienda" type="text" name="azienda" placeholder="Inserisci l'azienda dell'offerta">
+            <button id="button" type="submit">Cerca</button>
+    </form> 
 </div>
 
 
