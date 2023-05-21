3e483b7f7889ca2c814ec43aff81a6e3e340a6f7
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-21 11:16:04 +0200

    add asset  sul percorso dell helper per immagini

--- resources/views/coupon.blade.php
+++ resources/views/coupon.blade.php
@@ -6,39 +6,44 @@
     
 
   
-<section class="product">
-  <div class="image">
-     @include('helpers/productImg', ['attrs' => 'imagefrm', 'imgFile' => $selOfferta->image])
+    <section class="product">
+    <div class="image">
+        @include('helpers/productImg', ['attrs' => 'imagefrm', 'imgFile' => $selOfferta->image])
     </div>
 
-  <div class="product-info">
-    <h2>{{$selOfferta->Oggetto}}</h2>
+    <div class="product-info">
+        <h2>{{$selOfferta->Oggetto}}</h2>
 
-    <p> Descrizione offerta: {{$selOfferta->DescrizioneOfferta}}</p>
+        <p> Descrizione offerta: {{$selOfferta->DescrizioneOfferta}}</p>
 
-    <p> Categoria: {{$selOfferta->Categoria}}</p>
+        <p> Categoria: {{$selOfferta->Categoria}}</p>
 
-    <p> Azienda: {{$selOfferta->Azienda}}</p>
+        <p> Azienda: {{$selOfferta->Azienda}}</p>
 
-    <p> Scadenza: {{$selOfferta->Scadenza}}</p>
+        <p> Scadenza: {{$selOfferta->Scadenza}}</p>
 
-    <p class="prezzo_originale"> Prezzo originale: {{$selOfferta->Prezzo}}€</p>
+        <p class="prezzo_originale"> Prezzo originale: {{$selOfferta->Prezzo}}€</p>
 
-    <p> Percentuale sconto: {{$selOfferta->PercentualeSconto}}%</p>
+        <p> Percentuale sconto: {{$selOfferta->PercentualeSconto}}%</p>
 
-    <p class="prezzo_scontato"> Prezzo scontato: {{$selOfferta->PrezzoScontato}}€</p>
+        @php
+            $prezzoScontato = $selOfferta->Prezzo - ($selOfferta->Prezzo * $selOfferta->PercentualeSconto / 100);
+        @endphp
 
-    <p> Luogo di fruizione: {{$selOfferta->Luogo}}</p>
+        <p class="prezzo_scontato"> Prezzo scontato: {{$prezzoScontato}}€</p>
 
-    <p> Modalità di fruizione: {{$selOfferta->Modalità}}</p>
+        <p> Luogo di fruizione: {{$selOfferta->Luogo}}</p>
 
-    <!-- <input class="button" href="{{route('home')}}"> Ottieni Coupon</button> -->
-    <div>
-        <a href="{{ route('login') }}" class="button">Ottieni Coupon</a>
+        <p> Modalità di fruizione: {{$selOfferta->Modalità}}</p>
+
+        <div>
+            <a href="{{ route('login') }}" class="button">Ottieni Coupon</a>
+        </div>
+
     </div>
-
-  </div>
 </section>
+
+
 
 
 
